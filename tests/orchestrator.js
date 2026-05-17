eb688cfca1c76c5f2817fc64ae966693458fba03
Tavares <tavares.gabriel.dev@gmail.com>
2026-05-17 11:37:58 -0300

    fix(tests): update retry logic in waitForWebServer function for improved service readiness checks

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,9 +2,13 @@ import retry from "async-retry";
 async function waitForAllServices() {
   await waitForWebServer();
   async function waitForWebServer() {
-    return retry(fetchStatusPage);
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
-    async function fetchStatusPage() {
+    async function fetchStatusPage(bail, tryNumber) {
+      console.log(tryNumber);
       const response = await fetch("http://localhost:3000/api/v1/status");
       if (!response.ok) {
         throw new Error(
