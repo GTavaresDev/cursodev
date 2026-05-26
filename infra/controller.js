f7738b0934bb21f74a014b54d32357331e170d03
Tavares <tavares.gabriel.dev@gmail.com>
2026-05-26 15:19:25 -0300

    feat: add migration handling with getPendingMigrations and runMigrations methods

--- infra/controller.js
+++ infra/controller.js
@@ -1,4 +1,6 @@
 import { InternalServerError, MethodNotAllowedError } from "infra/erros.js";
+import migrator from "models/migrator.js";
+
 function OnNoMatchHandler(req, res) {
   const publicErrorMessage = new MethodNotAllowedError();
   return res.status(publicErrorMessage.statusCode).json(publicErrorMessage);
@@ -13,10 +15,29 @@ function onErrorHandler(err, req, res) {
   return res.status(publicErrorMessage.statusCode).json(publicErrorMessage);
 }
 
+async function getPendingMigrations(req, res) {
+  const pendingMigrations = await migrator.getPendingMigrations();
+  return res.status(200).json(pendingMigrations);
+}
+
+async function runMigrations(req, res) {
+  const createdMigrations = await migrator.runMigrations();
+
+  if (!createdMigrations || createdMigrations.length === 0) {
+    return res.status(200).json(createdMigrations);
+  }
+
+  return res.status(201).json(createdMigrations);
+}
+
 const controller = {
   errorHandlers: {
     onNoMatch: OnNoMatchHandler,
     onError: onErrorHandler,
+  },
+  migrations: {
+    getPendingMigrations,
+    runMigrations,
   },
 };
 export default controller;
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,63 +1,9 @@
-import { runner as migrationRunner } from "node-pg-migrate";
-import { join } from "node:path";
-import database from "infra/database.js";
 import { createRouter } from "next-connect";
 import controller from "infra/controller.js";
 
 const router = createRouter();
 
-router.get(getHandler);
-router.post(postHandler);
+router.get(controller.migrations.getPendingMigrations);
+router.post(controller.migrations.runMigrations);
 
 export default router.handler(controller.errorHandlers);
-
-async function getHandler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  const dbClient = await database.getNewClient();
-
-  try {
-    const pendingMigrations = await migrationRunner({
-      dbClient,
-      dryRun: true,
-      noLock: true,
-      dir: join(process.cwd(), "infra/migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-
-    return res.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
-}
-
-async function postHandler(req, res) {
-  if (req.method === "POST") {
-    const dbClient = await database.getNewClient();
-
-    try {
-      const createdMigrations = await migrationRunner({
-        dbClient,
-        dryRun: false,
-        noLock: true,
-        dir: join(process.cwd(), "infra/migrations"),
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations",
-      });
-
-      if (!createdMigrations || createdMigrations.length === 0) {
-        return res.status(200).json(createdMigrations);
-      }
-      return res.status(201).json(createdMigrations);
-    } finally {
-      await dbClient.end();
-    }
-  }
-
-  return res.status(405).json({ error: "Method not allowed" });
-}
